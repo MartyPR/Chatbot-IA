338749517098d5d3d6f8965b9ef4217ba4c3f511
MartyPR <martin.p1131@gmail.com>
2024-07-08 12:48:09 -0500

    add backend with openai, with express and dotenv

---
+++ Backend/app.js
@@ -0,0 +1,44 @@
+//sk-proj-sryAMpg8CawsXPilrGDQT3BlbkFJ5tZ0uEswaXfVT1A1Q2Jk
+
+import OpenAI from "openai";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+//authentciated with our api
+const openai = new OpenAI({
+  apiKey: process.env.API_KEY,
+});
+
+const app = express();
+const PORT = process.env.PORT || 9090;
+//! passin incoming
+app.use(express.json());
+//!Global variable to honld the conversation hisztory
+let conversationHistory = [
+  {
+    role: "system",
+    content: "You are a helpful assitant",
+  },
+];
+//!router
+app.post("/ask", async (req, res) => {
+  const userMessage = req.body.message;
+
+  //update the conversation history with the user's message
+  conversationHistory.push({ role: "user", content: userMessage });
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: conversationHistory,
+      model: "gpt-3.5-turbo",
+    });
+    //!Extract the response
+    const botResponse = completion.choices[0].message.content;
+    res.json({ message: botResponse });
+  } catch (error) {
+    res.status(500).send("Error generating response from openAi");
+  }
+});
+
+app.listen(PORT, console.log(`Server is Runnig ${PORT}`));
+//!run
